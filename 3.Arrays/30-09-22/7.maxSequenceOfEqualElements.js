26b55a525c9f1151f2d8bd45d03ae9a6e39e8d59
Georgi Georgiev <gogolino1982@gmail.com>
2022-10-08 13:35:04 +0300

    update 7

--- 3.Arrays/30-09-22/7.maxSequenceOfEqualElements.js
+++ 3.Arrays/30-09-22/7.maxSequenceOfEqualElements.js
@@ -1,20 +1,24 @@
 function maxElements(array) {
-    let equalArray = [];
     let length = array.length;
+    let longestEqual = 1;
+    let count = 1;
     let result = '';
+
     for (let i = 0; i < length; i++) {
-        let current = i;
-        for (let j = i + 1; j < length; j++) {            
-            if (array[i] !== array[j]){
-                break;    
+        let curentResult = array[i] + ' ';
+        for (let j = i + 1; j < length; j++) {           
+            if (array[j] === array[i]) {
+                count++;
+                curentResult += array[j] +' ';
+            } else {
+                count = 1;
+                break;
             }
-            if (array[i] === array[j]){
-                current += i;
-                
-            }equalArray.push(current[i]);
-                       
+            if (count > longestEqual) {
+                longestEqual = count;
+                result = curentResult ;
+            }
         }
-    }
- console.log(equalArray);
+    }console.log(result);
 }
 maxElements([2, 1, 1, 2, 3, 3, 2, 2, 2, 1])
